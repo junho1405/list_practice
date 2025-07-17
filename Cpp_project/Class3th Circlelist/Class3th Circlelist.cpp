397d43eaec00bf4f17fb8e6e448c270992e6d97f
junho1405 <junho1405@naver.com>
2025-07-17 15:44:06 +0900

    2025.07.17 deque

---
+++ Cpp_project/Class3th Circlelist/Class3th Circlelist.cpp
@@ -0,0 +1,147 @@
+﻿#include<iostream>
+using namespace std;
+template <typename T>
+
+class List
+{
+private:
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+    Node* head;
+    int size;
+public:
+    List()
+    {
+        size = 0;
+        head = nullptr;
+    }
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+        newNode->data = data;
+        if (head == nullptr)
+        {
+            head = newNode;
+            newNode->next = head;
+            cout << data << endl;
+        }
+        else
+        {
+            newNode->next = head->next;
+            head->next = newNode;
+            head = newNode;
+            cout << data << endl;
+        }
+        size++;
+        cout << head << endl;
+    }
+    void push_front(T data)
+    {
+        Node* newNode = new Node;
+        newNode->data = data;
+        if (head == nullptr)
+        {
+            head = newNode;
+            newNode->next = head;
+            cout << data << endl;
+        }
+        else//newNode->head
+        {
+            newNode->next = head->next;
+            head->next = newNode;
+            cout << data << endl;
+        }
+        size++;
+        cout << head << endl;
+    }
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "linked list is empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head->next;
+            if (head == head->next)
+            {
+                head = nullptr;
+            }
+            else
+            {
+                head->next = deleteNode->next;
+            }
+            cout << "삭제된 값:" << deleteNode->data << endl;
+            delete deleteNode;
+        }
+
+    }
+    void pop_back()
+    {
+        if (head == nullptr)
+        {
+            cout << "linked list is empty" << endl;
+            return;
+        }
+
+
+
+        if (head == head->next)//하나일때
+        {
+            cout << "삭제된 값:" << head->data << endl;
+            delete head;
+            head = nullptr;
+        }
+        else//둘 이상일 때
+        {
+            Node* current = head;
+            Node* deleteNode = head;
+            Node* previous = head;
+
+            //for (int i = 0; i < size; i++)
+            //{
+            //    current = current->next;
+            //}
+            //current->next = head->next;
+            //cout << "삭제된 값: " << current->data << endl;
+            //delete deleteNode;
+
+            while(current->next != head)
+            {
+            	previous = current;
+            	current = current->next;
+            }
+            previous->next = head;
+            delete previous;
+        }
+        size--;
+    }
+    bool empty()
+    {
+        return(head == nullptr);
+    }
+
+    ~List()
+    {
+        while (head != nullptr)
+        {
+            pop_front();
+            size--;
+        }
+    }
+};
+
+
+int main()
+{
+    List<int> list;
+    list.push_front(10);
+    list.push_front(20);
+    list.push_back(50);
+    list.push_back(40);
+    list.pop_front();
+    list.pop_back();
+}
--- Cpp_project/Cpp_project/Cpp_project.cpp
+++ Cpp_project/Cpp_project/Cpp_project.cpp
@@ -1,89 +1,91 @@
 ﻿#include<iostream>
-using namespace std;
-template <typename T>
 
-class List
+using namespace std;
+template<typename T>
+
+//벡터의 크기 
+//벡터 저장가능 메모리공간
+//동적 배열을 하기 위한 포인터 변수
+//생성자까지 모두 적을것
+
+class Vector
 {
 private:
-	struct Node
-	{
-		T data;
-		Node* next;
-		
+    int size;
+    int capacity;
+    T* container;
 
-	};
-	Node* head;
-	int size;
 public:
-	List()
-	{
-		size = 0;
-		head = nullptr;
-	}
-	void push_back(T data)
-	{
-		Node* newNode = new Node; 
-		newNode->data = data;
-		if (head == nullptr)
-		{
-			head = newNode;
-			newNode->next = head;
-			cout<<data<<endl;
-		}
-		else
-		{
-			newNode->next = head->next;
-			head->next = newNode;
-			head = newNode;
-			cout<<data<<endl;
-		}
-		size++;
-		cout << head << endl;
-	}
-	void push_front(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-		if (head == nullptr)
-		{
-			head = newNode;
-			newNode->next = head;
-			cout << data << endl;
-		}
-		else//newNode->head
-		{
-			newNode->next = head->next;
-			head->next = newNode;
-			cout << data << endl;
-		}
-		size++;
-		cout << head << endl;
-	}
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "linked list is empty" << endl;
-		}
-		Node* deleteNode = head;
-		if (head == deleteNode)
-		{
-			cout << "삭제된 값:" << deleteNode->data << endl;
-			delete deleteNode;
-			size--;
-		}
-		else
-		{
+    Vector() 
+    {
+        size = 0;
+        capacity = 0;
+        container = nullptr;
+    }
+    ~Vector()
+    {
+        if (container != nullptr)
+        {
+            delete[]container;
+        }
+    }
+    void resize(int newSize)
+    {
+        // 1. capacity에 새로운 size값을 저장 v
+        capacity = newSize;
 
-		}
-	}
+        // 2. 새로운 포인터 변수를 생성, 새롭게 만들어진 메모리 공간을 가리키도록 합니다.
+        T* newContainer = new T[capacity];
+        // 3. 새로운 메모리 공간의 값을 초기화합니다.
+        for (int i = 0; i < size; i++)
+        {
+            newContainer[i] = NULL;
+        }
+        // 4. 기존 배열에 있는값을 복사해서 새로운 배열에 넣어준다.
+        for (int i = 0; i < size; i++)
+        {
+            newContainer[i] = container[i];
+        }
+        // 5. 기존배열의 메모리 해제
+        if (container != nullptr)
+        {
+            delete[] container;
+        }
+        // 6. 기존 배열을 가르키던 포인터 변수의 값을 새로운 배열의 시작주소로 가르킨다.
+        container = newContainer;
+        }
+    void push_back(T data)
+    {
+        if (size >= capacity)
+        {
+            int newCapacity = capacity * 2;
+            resize(newCapacity);
+        }
+        container[size++] = data;
+        cout << "들어간 값 : " << data << endl;
+        cout << "새로 갱신된 최대치 : " << capacity << endl;
+        cout << "배열의 값 : {";
+        for (int i = 0; i < size; i++)
+        {
+            cout << container[i];            
+        }
+        cout << "}\n";
+        cout << "최종 size :"<<size<<endl;
+        cout << "최종 capacity :"<<capacity<<endl;
+    }
 };
 
 int main()
 {
-	List<int> list;
-	list.push_front(10);
-	list.push_front(20);
-	list.push_back(30);
-	list.pop_front();
+    Vector<int> vec;
+    vec.resize(1);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3); 
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3); 
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
 }
